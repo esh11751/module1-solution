04af10fbb43d5ae53578a2dff97eefeda6312ece
Dinne <Dinne@eshangu.com>
2016-09-18 18:04:02 -0500

    nextversion

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 (function () {
-  'use strict';
+  "use strict";
 
-  angular.module('LunchCheckApp', [])
-  .controller('LunchCheckController', functions ($scope) {
-    $scope.uname = "Eshwar";
+  angular.module("LunchCheckApp", [] )
+  .controller("LunchCheckController", function ($scope) {
+    $scope.lunchlist = "";
+    $scope.checkStatusVal = "";
+    $scope.checkListItems= fucntion (){
+      var msg  = getcheckmessage($scope.lunchlist);
+      $scope.checkStatusVal = msg;
+    };
+
+    fucntion getcheckmessage(splitstring){
+      var finalmsg = "Please enter data first";
+      var splitlist = splitstring.split(',');
+
+      if( splitlist.length==0 || splitstring="" ){
+        finalmsg = "Please enter data first"
+      }
+      if( splitlist.length<=3){
+        finalmsg = "Enjoy!";
+      }
+      if(splitlist.length>3){
+        finalmsg = "Too much!";
+      }
+      return finalmsg;
+    }
+
   });
-
 })();
